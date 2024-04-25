9282b7cc34a07bec6b67e328c6c75ba48c1706c9
Viet Truong <truong.viet1903@gmail.com>
2024-04-24 21:40:45 -0600

    first commit

---
+++ actions/deleteTaskAction.js
@@ -0,0 +1,25 @@
+'use server'
+import {revalidatePath} from 'next/cache'
+
+import {ref, remove} from 'firebase/database'
+
+import {db} from '@/lib/firebase/firebaseInit'
+
+export async function deleteAction(prevState, formData) {
+	const uid = formData.get('uid')
+	const response = await removeFromRTDB(uid)
+	revalidatePath('/demo')
+	return {message: response}
+}
+
+
+async function removeFromRTDB(uid) {
+	const path = `todosDB/${uid}`
+	const dbRef = ref(db, path)
+	try {
+		await remove(dbRef)
+		return 'success'
+	} catch (error) {
+		return 'failure'
+	}
+}
